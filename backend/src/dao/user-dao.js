525dbc9ea8466d9d36f3c4da3226d545a157d565
Darius96 <dariustinculescu@gmail.com>
2022-07-20 06:35:55 +0300

    Created rest api for deleting users

--- backend/src/dao/user-dao.js
+++ backend/src/dao/user-dao.js
@@ -3,11 +3,18 @@ const Encrypt = require('../security/bcrypt-utils');
 
 module.exports = {
     find: async (criteria) => await find(criteria),
-    save: async (user) => await save(user)
+    save: async (user) => await save(user),
+    deleteByEmail: async (email) => await deleteByEmail(email)
 }
 
 async function find(criteria) {
     return User.find(criteria);
+}
+
+function deleteByEmail(email) {
+    console.log('----------------Trying to delete user by email with param: email = %s', email);
+
+    return User.deleteOne({email: {$eq: email}});
 }
 
 async function save(user) {
