02c5f6339f6a648af67a40d4cf0390a5ecada596
Darius96 <dariustinculescu@gmail.com>
2022-07-20 03:32:54 +0300

    Added UserList component (dummy data) && removed unused styles from index.css

---
+++ client/haufe-app/src/components/users/UserList.js
@@ -0,0 +1,13 @@
+import './UserList.scss'
+import Header from "../header/Header";
+
+export default function UserList() {
+    return (
+        <main>
+            <Header/>
+            <section className='container'>
+                <h1>User list placeholder</h1>
+            </section>
+        </main>
+    )
+}
